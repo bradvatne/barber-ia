315bff23c6dc2e9d9f8005e370c48968d4d3eb40
Brad Vatne <bradvatne@gmail.com>
2020-10-09 20:14:39 +0800

    Updates

---
+++ precache-manifest.a18e40e5fc9743431899453d1a5d9f9d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5043b299c52d15abc4e784057ff7a17",
+    "url": "/barber-ia/index.html"
+  },
+  {
+    "revision": "9dda50200480abfb6e13",
+    "url": "/barber-ia/static/css/main.d2bdb7db.chunk.css"
+  },
+  {
+    "revision": "106486e898b30c30e1e3",
+    "url": "/barber-ia/static/js/2.56e5da2a.chunk.js"
+  },
+  {
+    "revision": "5395743a0cde72965dfb2a358dfa6b63",
+    "url": "/barber-ia/static/js/2.56e5da2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dda50200480abfb6e13",
+    "url": "/barber-ia/static/js/main.00be5de9.chunk.js"
+  },
+  {
+    "revision": "30f942bb7783c4b076a0017ec0876fab",
+    "url": "/barber-ia/static/js/main.00be5de9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d581345aca7abb13a61",
+    "url": "/barber-ia/static/js/runtime-main.30d522f0.js"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/barber-ia/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/barber-ia/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "f26c34bb88eb83d5aae99c7dbf3ddd48",
+    "url": "/barber-ia/static/media/fabio-mangione.f26c34bb.jpg"
+  },
+  {
+    "revision": "b852b4e14e04bd9278774e44200dba82",
+    "url": "/barber-ia/static/media/headshot.b852b4e1.png"
+  },
+  {
+    "revision": "01f1f2d0f4a22129602734c323a6e03e",
+    "url": "/barber-ia/static/media/hero-image-2.01f1f2d0.jpg"
+  },
+  {
+    "revision": "2838c7671ed681000124d94724971e54",
+    "url": "/barber-ia/static/media/hero-logo-blue.2838c767.png"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/barber-ia/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "be9fa468a029a195601370492c15519d",
+    "url": "/barber-ia/static/media/login-image.be9fa468.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/barber-ia/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/barber-ia/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/barber-ia/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/barber-ia/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
